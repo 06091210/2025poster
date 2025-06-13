57a0316a7fb2bbec94886802b1453f54b3982c61
06091210 <firp771@gmail.com>
2025-06-13 20:58:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-npm install ws
 const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 8080 });
 
 server.on('connection', (socket) => {
-  console.log('新しい接続がありました！');
+    console.log('新しい接続がありました！');
 
-  socket.on('message', (message) => {
-    console.log(`受信メッセージ: ${message}`);
-    socket.send(`あなたが送ったメッセージ: ${message}`);
-  });
-
-  socket.on('close', () => {
-    console.log('接続が閉じられました');
-  });
+    socket.on('message', (message) => {
+        console.log(`受信メッセージ: ${message}`);
+        socket.send(`あなたが送ったメッセージ: ${message}`);
+    });
 });
 
 console.log('WebSocketサーバーが8080番ポートで起動しました。');
