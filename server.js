8b87414fef8d89ba9837d6a661105c655147c67e
06091210 <firp771@gmail.com>
2025-06-13 20:51:16 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket) => {
+  console.log('新しい接続がありました！');
+
+  socket.on('message', (message) => {
+    console.log(`受信メッセージ: ${message}`);
+    socket.send(`あなたが送ったメッセージ: ${message}`);
+  });
+
+  socket.on('close', () => {
+    console.log('接続が閉じられました');
+  });
+});
+
+console.log('WebSocketサーバーが8080番ポートで起動しました。');
